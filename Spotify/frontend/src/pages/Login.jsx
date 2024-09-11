3fd737db04d9961bd6b010561a25c0575b6be69b
Alphteow <e0540502@u.nus.edu>
2024-09-11 11:47:57 +0800

    Added register button to login page

--- Spotify/frontend/src/pages/Login.jsx
+++ Spotify/frontend/src/pages/Login.jsx
@@ -1,7 +1,29 @@
-import Form from "../components/Form"
+// import Form from "../components/Form"
+
+// function Login() {
+//     return <Form route="/api/token/" method="login" />
+// }
+
+// export default Login
+
+import React from "react";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
+import Form from "../components/Form";
 
 function Login() {
-    return <Form route="/api/token/" method="login" />
+    return (
+        <div>
+            <Form route="/api/token/" method="login" />
+
+            {/* Add a button linking to the register page */}
+            <div className="register-link">
+                <p>Don't have an account?</p>
+                <Link to="/register">
+                    <button>Register</button>
+                </Link>
+            </div>
+        </div>
+    );
 }
 
-export default Login
+export default Login;
